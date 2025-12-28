d27c658b2a4824bb1fcd21c9ceb96f998ee8d398
Anand Kumar Gupta <kumarguptaanand1@gmail.com>
2025-12-28 20:40:38 +0530

    JPA repo

---
+++ src/main/java/com/example/MyProject/Controller/UserController.java
@@ -0,0 +1,41 @@
+package com.example.MyProject.Controller;
+
+import com.example.MyProject.Entity.User;
+import com.example.MyProject.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // POST - save user
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    // GET - all users
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    // GET - user by id
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    // DELETE - user
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return "User deleted";
+    }
+}
---
+++ src/main/java/com/example/MyProject/Service/UserService.java
@@ -0,0 +1,35 @@
+package com.example.MyProject.Service;
+
+import com.example.MyProject.Entity.User;
+import com.example.MyProject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Save user
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // Get all users
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Get user by id
+    public User getUserById(int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    // Delete user
+    public void deleteUser(int id) {
+        userRepository.deleteById(id);
+    }
+}
